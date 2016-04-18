64cf788bad6576ce910472501f4ba402a0d7be38
Aaron Schmohe <schmo-dog@users.noreply.github.com>
2016-04-17 21:42:05 -0600

    Changed up min amount of pizzas updated

--- views/js/main.js
+++ views/js/main.js
@@ -540,7 +540,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 24; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
